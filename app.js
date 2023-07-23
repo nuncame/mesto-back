60cb0d1fd682f5f86f1a8d09b15c41b234bd986f
Victoria Markova <vii.nuncame@gmail.com>
2023-07-23 22:24:00 +0300

    fix: review

--- app.js
+++ app.js
@@ -56,16 +56,16 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[a-zA-Z0-9-\._~:\/\?#[\]@!\$&'()\*\+,;=]{2,256}\.[a-z]{2,4}\b([a-zA-Z0-9-\._~:\/\?#[\]@!\$&'()\*\+,;=]*)/),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]{2,256}\.[a-z]{2,4}\b([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(3),
-  }).unknown(true),
+  }),
 }), createUser);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(3),
-  }).unknown(true),
+  }),
 }), login);
 
 app.use(authorize);
